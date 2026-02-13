ac2c1ab7fdfd202b1682e888a6e24e04c802c17a
liuboping <liuboping@elex-tech.com>
2026-02-14 02:06:01 +0800

    Fix v1.0.13 URL and SHA256

--- Formula/boltdb.rb
+++ Formula/boltdb.rb
@@ -6,21 +6,21 @@ class Boltdb < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lbp0200/BoltDB/releases/download/1.0.13/boltDB-1.0.13-darwin-arm64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/lbp0200/BoltDB/releases/download/v1.0.13/boltDB-1.0.13-darwin-arm64"
+      sha256 "18b687403fc90685fc4385a40fea59ca7becf00f0badef90020b0255baea7480"
     else
-      url "https://github.com/lbp0200/BoltDB/releases/download/1.0.13/boltDB-1.0.13-darwin-amd64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/lbp0200/BoltDB/releases/download/v1.0.13/boltDB-1.0.13-darwin-amd64"
+      sha256 "10e58dcf7d10984f88a5ced38aadc2ec8e45bca92d40f6d15fd090cb843dc0b7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/lbp0200/BoltDB/releases/download/1.0.13/boltDB-1.0.13-linux-arm64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/lbp0200/BoltDB/releases/download/v1.0.13/boltDB-1.0.13-linux-arm64"
+      sha256 "9a769a9c837c17eb762e17253e19fa27e8203e4d78bee266ff1ffea9daea66cd"
     else
-      url "https://github.com/lbp0200/BoltDB/releases/download/1.0.13/boltDB-1.0.13-linux-amd64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/lbp0200/BoltDB/releases/download/v1.0.13/boltDB-1.0.13-linux-amd64"
+      sha256 "b9b08c99c323598ee8ee0a383f55d36fffab6c3b807d1a5057897b837b78c00f"
     end
   end
 
